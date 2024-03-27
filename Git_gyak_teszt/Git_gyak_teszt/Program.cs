c576c343cd82daaf840f4233c1194015ec3c6e96
unknown <szombathdomi@stud.uni-obuda.hu>
2024-03-27 16:17:20 +0100

    added empty console project

---
+++ Git_gyak_teszt/Git_gyak_teszt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Git_gyak_teszt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
